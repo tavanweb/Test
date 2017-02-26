dac06cb398489f21d565c25eddc509cf5fd8169a
yozar30f <yozar30f@users.noreply.github.com>
2017-02-27 01:17:19 +0330

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,32 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {
+    ["-1001093583043"] = false
+  },
+  enabled_plugins = {
+    "banhammer",
+    "banhammer-fa",
+    "groupmanager",
+    "groupmanager-fa",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "expiretime",
+    "mute-time",
+    "del",
+    "lock-fosh",
+    "BotOn_Off",
+    "time",
+    "azan",
+    "monshi"
+  },
+  info_text = "》permag.ir v3\n\n🔰Our channel :\n》@permag_bots《\n\n🔰Our channel2 :\n》@permag_ir《\n\n🔰Our channel3 :\n》@permag《\n\n🔰Our website :\n》http://permag.ir\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    153616907
+  }
+}
+return _
+end
